4eae1562eac0d349fb2f2650c1ce6673bb1310af
Christopher O'Driscoll <77005138+krilleellerstoffe@users.noreply.github.com>
2023-10-24 10:20:50 +0200

    read random names with pack uri

--- WPFBlackjack/InsertName.xaml.cs
+++ WPFBlackjack/InsertName.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,26 @@ namespace WPFBlackjack
         public InsertName()
         {
             InitializeComponent();
+        }
+
+        private void btnRandom_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("pack://application:,,,/Resources/RandomNames.txt");
+            var info = Application.GetResourceStream(uri);
+
+            if (info != null)
+            {
+                using (var reader = new StreamReader(info.Stream))
+                {
+                    string text = reader.ReadToEnd();
+
+                    // Split the text into lines
+                    string[] lines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                    string newRandomName = lines[new Random().Next(lines.Length)];
+                    txtName.Text = newRandomName.Trim(); ;
+                }
+            }
+            
         }
     }
 }
